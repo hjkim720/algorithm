6df1965cf915134df6bd950c3a2cc08fda62f0cb
hjkim720 <joonbutjuly@gmail.com>
2025-03-09 21:35:24 +0900

    [Silver I] Title: 정수 삼각형, Time: 116 ms, Memory: 111668 KB -BaekjoonHub

---
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.py
@@ -0,0 +1,12 @@
+N = int(input())
+triangle = [list(map(int, input().split())) for _ in range(N)]
+
+for i in range(1, N):
+    for j in range(i + 1):
+        if j == 0:  
+            triangle[i][j] += triangle[i - 1][j]
+        elif j == i:  
+            triangle[i][j] += triangle[i - 1][j - 1]
+        else: 
+            triangle[i][j] += max(triangle[i - 1][j - 1], triangle[i - 1][j])
+print(max(triangle[-1]))
