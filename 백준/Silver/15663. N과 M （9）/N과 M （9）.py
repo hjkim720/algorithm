f6fc4e318cb709d99de36aa413926db90625603f
hjkim720 <joonbutjuly@gmail.com>
2025-03-12 13:52:20 +0900

    [Silver II] Title: N과 M (9), Time: 272 ms, Memory: 117284 KB -BaekjoonHub

---
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.py
@@ -0,0 +1,25 @@
+N,M=map(int,input().split())
+lst=list(map(int,input().split()))
+res=[]
+lst.sort()
+cnt_dic={}
+final=set()
+for i in lst:
+    if i in cnt_dic:
+        cnt_dic[i]+=1
+    else:
+        cnt_dic[i]=1
+def perm(cnt):
+    if cnt==M:
+        final.add(tuple(res))
+        return
+    for items in lst:
+        if cnt_dic[items]!=0:
+            cnt_dic[items]-=1
+            res.append(items)
+            perm(cnt+1)
+            res.pop()
+            cnt_dic[items]+=1
+perm(0)
+for items in final:
+    print(*items)
