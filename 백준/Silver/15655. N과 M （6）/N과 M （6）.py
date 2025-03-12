c2fbba765f1be860d66e16a12584f19f44b7234d
hjkim720 <joonbutjuly@gmail.com>
2025-03-12 12:59:42 +0900

    [Silver III] Title: N과 M (6), Time: 92 ms, Memory: 109544 KB -BaekjoonHub

---
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.py
@@ -0,0 +1,25 @@
+N,M=map(int,input().split())
+lst=list(map(int,input().split()))
+res=[]
+lst.sort()
+visited=[0]*(max(lst)+1)
+def perm(cnt):
+    if cnt==M:
+        print(*res)
+        return
+    for items in lst:
+        if not visited[items]:
+            if not res:
+                visited[items]=1
+                res.append(items)
+                perm(cnt+1)
+                res.pop()
+                visited[items]=0
+            else:
+                if items>res[-1]:
+                    visited[items] = 1
+                    res.append(items)
+                    perm(cnt + 1)
+                    res.pop()
+                    visited[items] = 0
+perm(0)
