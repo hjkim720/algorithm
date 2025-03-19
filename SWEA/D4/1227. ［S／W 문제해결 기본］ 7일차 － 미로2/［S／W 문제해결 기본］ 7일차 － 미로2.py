ff370dd6bf62e203a0a415b63f59184c8ba252d0
hjkim720 <joonbutjuly@gmail.com>
2025-03-19 09:48:20 +0900

    [D4] Title: [S/W 문제해결 기본] 7일차 - 미로2, Time: 146 ms, Memory: 62,080 KB -BaekjoonHub

---
+++ SWEA/D4/1227. ［S／W 문제해결 기본］ 7일차 － 미로2/［S／W 문제해결 기본］ 7일차 － 미로2.py
@@ -0,0 +1,40 @@
+from collections import deque
+dir=((1,0),(0,1),(-1,0),(0,-1))
+def find_start_end():
+    global found_start,found_end
+    start=0
+    end=0
+    for i in range(100):
+        for j in range(100):
+            if maze[i][j]==2:
+                start = (i,j)
+                found_start = True
+            elif maze[i][j]==3:
+                end = (i,j)
+                found_end=True
+        if found_start and found_end:
+            break
+    return start,end
+def bfs(start,end):
+    q=deque()
+    q.append(start)
+    visited=[[0]*100 for _ in range(100)]
+    visited[start[0]][start[1]]=1
+    while q:
+        ti,tj=q.popleft()
+        if (ti,tj)==end:
+            return 1
+        for d in dir:
+            wi,wj=ti+d[0],tj+d[1]
+            if 0<=wi<100 and 0<=wj<100 and maze[wi][wj]!=1 and visited[wi][wj]==0:
+                q.append((wi,wj))
+                visited[wi][wj]=1
+    return 0
+
+for testcase in range(1,11):
+    N=int(input())
+    maze=[list(map(int,input())) for _ in range(100)]
+    found_start = False
+    found_end = False
+    start,end=find_start_end()
+    print(f'#{testcase} {bfs(start,end)}')
