eee97f34ddbab1b70dcf62911a91ed7b0b8d0e48
hjkim720 <joonbutjuly@gmail.com>
2025-03-23 21:28:05 +0900

    [Platinum V] Title: 두더지가 정보섬에 올라온 이유, Time: 376 ms, Memory: 130112 KB -BaekjoonHub

---
+++ 백준/Platinum/17132. 두더지가 정보섬에 올라온 이유/두더지가 정보섬에 올라온 이유.py
@@ -0,0 +1,31 @@
+import sys
+input=sys.stdin.readline
+def find(x):
+    if parent[x]!=x:
+        parent[x]=find(parent[x])
+    return parent[x]
+def union(x,y):
+    ref_x=find(x)
+    ref_y=find(y)
+    if ref_x==ref_y:
+        return
+    else:
+        if size[ref_x]>size[ref_y]:
+            ref_x,ref_y=ref_y,ref_x
+        size[ref_x]+=size[ref_y]
+        parent[ref_y]=ref_x
+N=int(input())
+parent=list(range(N+1))
+info=[]
+for _ in range(N-1):
+    temp=list(map(int,input().split()))
+    info.append(temp)
+info.sort(key=lambda x:x[2], reverse=True)
+res=0
+size=[1]*(N+1)
+for u,v,weight in info:
+    ru,rv=find(u),find(v)
+    if ru!=rv:
+        res+=weight*size[ru]*size[rv]
+        union(u,v)
+print(res)
