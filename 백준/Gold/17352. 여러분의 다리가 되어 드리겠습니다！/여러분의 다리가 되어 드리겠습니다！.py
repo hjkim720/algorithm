e19f957fa67c1249ee02de629378e4e8b0b8aad9
hjkim720 <joonbutjuly@gmail.com>
2025-03-24 09:26:43 +0900

    [Gold V] Title: 여러분의 다리가 되어 드리겠습니다!, Time: 448 ms, Memory: 115208 KB -BaekjoonHub

---
+++ 백준/Gold/17352. 여러분의 다리가 되어 드리겠습니다！/여러분의 다리가 되어 드리겠습니다！.py
@@ -0,0 +1,21 @@
+def find(x):
+    if parent[x]!=x:
+        parent[x]=find(parent[x])
+    return parent[x]
+def union(x,y):
+    ref_x=find(x)
+    ref_y=find(y)
+    if ref_x==ref_y:
+        return
+    elif ref_x<ref_y:
+        parent[ref_y]=ref_x
+    else:
+        parent[ref_x]=ref_y
+N=int(input())
+parent=list(range(N+1))
+for _ in range(N-2):
+    u,v=map(int,input().split())
+    if find(u)!=find(v):
+        union(u,v)
+res=set(find(x) for x in range(1, N+1))
+print(*res)
