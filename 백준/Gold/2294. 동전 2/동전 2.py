3fcea55a4961f7df22cf0f0708775a4e8a4caea3
hjkim720 <joonbutjuly@gmail.com>
2025-03-26 22:19:31 +0900

    [Gold V] Title: 동전 2, Time: 136 ms, Memory: 34916 KB -BaekjoonHub

--- 백준/Gold/2294. 동전 2/동전 2.py
+++ 백준/Gold/2294. 동전 2/동전 2.py
@@ -1,3 +1,7 @@
+# 문제를 보자마자 BFS가 떠올랐음(추후 DP로 다시 풀어볼 예정)
+# 왜 BFS가 떠올랐냐? ->최소 갯수 = 최단경로
+# 시작 위치를 0으로 두고 타겟인 K로 출발
+# 한번에 coins 리스트(입력받은 coin의 종류)에 있는 coin칸을 갈 수 있다고 해석할 수 있음
 from collections import deque
 import sys
 input=sys.stdin.readline
