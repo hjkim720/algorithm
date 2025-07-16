7ac782efafb6d69085f592fe3281d41b8a74a2cc
hjkim720 <joonbutjuly@gmail.com>
2025-07-16 14:29:24 +0900

    [Gold II] Title: 보석 도둑, Time: 1500 ms, Memory: 204024 KB -BaekjoonHub

---
+++ 백준/Gold/1202. 보석 도둑/보석 도둑.py
@@ -0,0 +1,21 @@
+import heapq
+N,K=map(int,input().split())
+bosuks=list(list(map(int,input().split())) for _ in range(N))
+bags=list(int(input()) for _ in range(K))
+bosuks.sort(reverse=True)
+# bags를 reverse=True 넣고 sort한게 원흉이었다.
+bags.sort()
+q=[]
+res=0
+for bag in bags:
+    while bosuks:
+        weight, cost=bosuks.pop()
+        if bag>=weight:
+            heapq.heappush(q,-cost)
+        else:
+            bosuks.append((weight,cost))
+            break
+    if q:
+        res-=heapq.heappop(q)
+print(res)
+
