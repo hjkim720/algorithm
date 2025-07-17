9c77e5050d50cf67020b91c9287de2b01ae3e8e4
hjkim720 <joonbutjuly@gmail.com>
2025-07-17 23:16:40 +0900

    [Gold I] Title: 이항 계수 3, Time: 1148 ms, Memory: 188288 KB -BaekjoonHub

---
+++ 백준/Gold/11401. 이항 계수 3/이항 계수 3.py
@@ -0,0 +1,16 @@
+'''
+FLT 오랜만..ㅎㅎ...증명 기말고사 나왔던게 어제 같구나
+'''
+import sys
+input = sys.stdin.readline
+MOD = 1000000007
+N, K = map(int, input().split())
+fact = [1] * (N + 1)
+for i in range(1, N + 1):
+    fact[i] = (fact[i-1] * i) % MOD
+def inv(a):
+    return pow(a, MOD-2, MOD)
+n = fact[N]
+d = (fact[K] * fact[N-K]) % MOD
+result = (n * inv(d)) % MOD
+print(result)
