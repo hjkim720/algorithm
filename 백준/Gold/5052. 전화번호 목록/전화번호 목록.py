b2ddc8b1359b527695300aac655ee526fa2bc022
hjkim720 <joonbutjuly@gmail.com>
2025-07-26 22:33:00 +0900

    [Gold IV] Title: 전화번호 목록, Time: 236 ms, Memory: 116564 KB -BaekjoonHub

---
+++ 백준/Gold/5052. 전화번호 목록/전화번호 목록.py
@@ -0,0 +1,11 @@
+T=int(input())
+for _ in range(T):
+    N=int(input())
+    lst=list((input().strip()) for _ in range(N))
+    lst.sort()
+    for i in range(N-1):
+        if lst[i+1].startswith(lst[i]):
+            print("NO")
+            break
+    else:
+        print("YES")
