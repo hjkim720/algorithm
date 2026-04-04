080937282ec8d7813130eb638d08ad10bb399e2b
Hyojoon Kim <joonbutjuly@gmail.com>
2026-04-05 00:39:42 +0900

    [Gold I] Title: 멀티탭 스케줄링, Time: 108 ms, Memory: 111308 KB -BaekjoonHub

---
+++ 백준/Gold/1700. 멀티탭 스케줄링/멀티탭 스케줄링.py
@@ -0,0 +1,25 @@
+from collections import deque
+N, K = map(int,input().split())
+lst=deque(map(int,input().split()))
+occupied=set()
+cnt=0
+for i in range(K):
+    tmp=lst.popleft()
+    if tmp in occupied: continue
+    if len(occupied)<N:
+        occupied.add(tmp)
+        continue
+    who=0
+    far=-1
+    for item in occupied:
+        if item not in lst:
+            who=item
+            break
+        tmp_far=lst.index(item)
+        if tmp_far>far:
+            far=tmp_far
+            who=item
+    occupied.remove(who)
+    occupied.add(tmp)
+    cnt+=1
+print(cnt)
